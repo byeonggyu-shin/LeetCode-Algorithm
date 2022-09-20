f01b9954de43ac25bfa7201d0a6940a73d2bdc4e
Byeonggyu Shin <bgshin4793@gmail.com>
2022-09-20 21:14:58 +0900

    58. Easy

---
+++ lengthOfLastWorld.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  let arr = s.split(' ')
+
+  if (arr[arr.length - 1] === '') {
+    let newArr = arr.filter((el) => el.length > 0)
+    return newArr[newArr.length - 1].length
+  }
+  return arr[arr.length - 1].length
+};
+
+console.log(lengthOfLastWord("Hello World"))
+console.log(lengthOfLastWord("   fly me   to   the moon  "))
+console.log(lengthOfLastWord("luffy is still joyboy"))
