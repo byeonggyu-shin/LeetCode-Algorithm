e1fa3d12540ed0a1961eab07ccb47b87b0d27ccf
Byeonggyu Shin <94808683+byeonggyu-shin@users.noreply.github.com>
2023-06-05 13:16:06 +0900

    sorting Lv2

--- Programmers/Sorting/H-index.js
+++ Programmers/Sorting/H-index.js
@@ -1,19 +1,28 @@
+// function solution(citations) {
+//   const sortedCitations = citations.sort((a, b) => a - b);
+//   let n = citations.length;
+//   let answer = n - 1;
+
+//   for (let i = 0; i < n; i++) {
+//     let h = sortedCitations[i];
+//     if (h >= n - i && h <= i + 1) {
+//       answer = i;
+//       break;
+//     }
+//   }
+
+//   for (let i = 0; i < n; i++) {
+//     if (citations[i] === sortedCitations[answer]) {
+//       return i;
+//     }
+//   }
+// }
+
 function solution(citations) {
-  const sortedCitations = citations.sort((a, b) => a - b);
-  let n = citations.length;
-  let answer = n - 1;
-
-  for (let i = 0; i < n; i++) {
-    let h = sortedCitations[i];
-    if (h >= n - i && h <= i + 1) {
-      answer = i;
-      break;
-    }
+  citations.sort((a, b) => b - a);
+  let i = 0;
+  while (i + 1 <= citations[i]) {
+    i++;
   }
-
-  for (let i = 0; i < n; i++) {
-    if (citations[i] === sortedCitations[answer]) {
-      return i;
-    }
-  }
+  return i;
 }
